4d827820bc244e7c3ac85d06e35bea81f07f6b6f
Oliver Rickfors <oliverrickfors@hotmail.com>
2019-03-17 22:43:29 +0100

    idk

--- param-extract.py
+++ param-extract.py
@@ -1,6 +1,9 @@
 import glob
 import sys
 import re
+from termcolor import colored
+import urllib.request
+
 
 def getParams(regexstr, content):
 	regexcomp = re.compile(r"" + regexstr +"")
@@ -8,13 +11,14 @@ def getParams(regexstr, content):
 	return params
 
 regexlist = []
+urlList = []
 
 with open("./regex", 'r') as f:
 	for line in f.readlines():
 		regexlist.append(line.rstrip())
 
 if len(sys.argv) >= 3:
-	wwwrootpath = sys.argv[1] +  '/**/*.*'	
+	wwwrootpath = sys.argv[1] +  '/**/*.*'
 	siteurl = sys.argv[2]
 	payload = sys.argv[3]
 
@@ -26,14 +30,34 @@ filepaths = glob.iglob((wwwrootpath), recursive=True)
 
 for filename in filepaths:
 	content = ""
-	print(filename)
 	with open(filename, 'r') as f:
 		for line in f.read():
 			content += line
+		
+		paramstr = ""
+		paramstrprint = ""
 
 	for x in range(0, len(regexlist)):
 		phpparams = getParams(str(regexlist[x]), content)
+		phpparams = set(phpparams)
 
 		for param in phpparams:
-			test = siteurl + filename + "?" + param[1] + "=" + payload
-			print(test)
+			paramstr += str(param[1]) + "=" + payload + "&"
+			paramstrprint += colored(str(param[1]), 'green') + "=" + payload + "&"
+	if paramstr:
+		url = siteurl + (filename.replace(sys.argv[1], "")) + "?" + paramstr[:-1]
+		print (siteurl + (filename.replace(sys.argv[1], "")) + "?" + paramstrprint[:-1])
+		urlList.append(url.rstrip())
+
+yesorno = input('Check for reflections? y/n')
+
+if yesorno == 'y':
+	for url in urlList:
+		try:
+			with urllib.request.urlopen(url) as response:
+			   	resp = response.read()
+			   	if payload in str(resp):
+			   		print(colored('Reflected input! ', 'green'), url)
+		except urllib.error.HTTPError as err:
+#   			if err.code == 404:
+   				print(colored(err.code, 'red'), url)
